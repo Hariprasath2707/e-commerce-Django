290bbdca68b34bd45bebb77151136fd376be0372
Hariprasath2707 <hariprasath642@gmail.com>
2024-12-24 12:02:32 +0530

    Initial commit

---
+++ shop/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from . models import *
+
+# Create your views here.
+
+def home(request):
+    return render(request, "shop/index.html")
+
+def register(request):
+    return render(request,"shop/register.html")
+
+def collections(request):
+    category = Category.objects.filter(status=0)
+    return render(request,"shop/collections.html",{"category":category})
